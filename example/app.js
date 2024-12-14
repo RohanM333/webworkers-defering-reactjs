0a4434c36e54f2d8e01f389d58be47ea3fdc4384
Rohan Majrikar <62887858+RohanM333@users.noreply.github.com>
2024-12-14 22:36:22 +0530

    Create app.js

---
+++ example/app.js
@@ -0,0 +1,11 @@
+// src/App.js
+import React from 'react';
+import MainComponent from './components/MainComponent';
+
+const App = () => (
+  <div>
+    <MainComponent />
+  </div>
+);
+
+export default App;
