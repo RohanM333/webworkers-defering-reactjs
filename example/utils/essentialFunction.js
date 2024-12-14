276d2176a8cc1e49d8df355200c16fbaf2d7e413
Rohan Majrikar <62887858+RohanM333@users.noreply.github.com>
2024-12-14 22:37:26 +0530

    Create essentialFunction.js

---
+++ example/utils/essentialFunction.js
@@ -0,0 +1,7 @@
+// src/utils/essentialFunction.js
+const essentialFunction = () => {
+  console.log('Essential function executed!');
+  alert('Essential function executed!');
+};
+
+export default essentialFunction;
